710c9d807b6ebdc81ebb377579b50455486de60b
Wyrafinowany <zajac_dominik@onet.pl>
2019-08-03 11:40:23 +0200

    Create navigation

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,13 +5,14 @@ import Logo from '../../Logo/Logo';
 
 /* Styles */
 import classes from './Toolbar.css';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 );
