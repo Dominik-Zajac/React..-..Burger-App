e584a869bad28798c80c6c322f133bb4fcc7754d
Wyrafinowany <zajac_dominik@onet.pl>
2019-08-06 08:41:54 +0200

    Add navigating to the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -119,38 +119,39 @@ class BurgerBuilder extends Component {
 
     // Continue order
     purchaseContinueHandler = () => {
-        this.setState({
-            loading: true
-        });
+        // this.setState({
+        //     loading: true
+        // });
 
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Dominik Zajac',
-                address: {
-                    street: 'Test',
-                    zipCode: '11111',
-                    country: 'Poland'
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        }
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Dominik Zajac',
+        //         address: {
+        //             street: 'Test',
+        //             zipCode: '11111',
+        //             country: 'Poland'
+        //         },
+        //         email: 'test@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({
-                    loading: false,
-                    purchasing: false,
-                });
-            })
-            .catch(error => {
-                this.setState({
-                    loading: false,
-                    purchasing: false,
-                });
-            })
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false,
+        //         });
+        //     })
+        //     .catch(error => {
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false,
+        //         });
+        //     })
+        this.props.history.push('/checkout');
     };
 
     render() {
